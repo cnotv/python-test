40c1f88165cbfcdef532344c6127230f762bcbd4
cnotv <giuseppe.leo@suse.com>
2025-01-01 02:02:50 +0100

    Initial commit

---
+++ calc.py
@@ -0,0 +1,9 @@
+# Function to sum two numbers
+def sum_two_numbers(a, b):
+  return a + b
+
+# Example usage
+num1 = 5
+num2 = 3
+result = sum_two_numbers(num1, num2)
+print(f"The sum of {num1} and {num2} is {result}")
